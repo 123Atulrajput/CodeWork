d82b503519f1c017f8105035db3f9cef0163bd9d
Atul Singh <91182252+CoderBannaa@users.noreply.github.com>
2023-10-01 00:19:58 +0530

    Added solution - LeetHub

--- Sorting Elements of an Array by Frequency - GFG/sorting-elements-of-an-array-by-frequency.cpp
+++ Sorting Elements of an Array by Frequency - GFG/sorting-elements-of-an-array-by-frequency.cpp
@@ -1,58 +1,55 @@
-#include<iostream>
+
 #include<vector>
 #include<queue>
 #include<unordered_map>
 using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-vector<int> sortByFreq(int arr[],int n)
-   {
-       //Your code here
-       vector<int>v;
-        unordered_map<int , int>mp;
-       priority_queue<pair<int , int>>maxh;
-       
-       for(int i = 0 ; i<n ; i++)
-       {
-           mp[arr[i]]++;
-       }
-       
-       for(auto it = mp.begin() ; it !=mp.end() ; it++)
-       {
-           maxh.push({it->second , -1*(it->first)});
-       }
-       while(!maxh.empty())
-       {
-           int fre = maxh.top().first;
-           int ele = maxh.top().second;
-           for(int i = 1 ; i<=fre ; i++)
-           {
-               v.push_back(-1*ele);
-           }
-               maxh.pop();
-       }
-       return v;
-   }
 
-int main()
-{
-//code
-int t;
-cin>>t;
-while(t--)
-{
-    int n;
-    cin>>n;
-    int a[n];
-    for(int i = 0 ; i<n ; i++)
-    {
-        cin>>a[i];
+vector<int>Sortbyfreq( int arr[],int n){
+    vector<int>v;
+    unordered_map<int ,int>mp;
+    priority_queue<pair<int , int >>pq;
+    
+    for( int i = 0 ; i<n ; i++){
+        mp[arr[i]]++;
+        
     }
-     vector<int>v1 = sortByFreq(a , n);
-     for(int i = 0 ; i<v1.size(); i++)
-     {
-         cout<<v1[i]<<" ";
-     }
-     cout<<endl;
+   
+    for(auto it : mp){
+            pq.push({it.second, -1*(it.first)}); 
+    }
+            
+    while(!pq.empty()){
+        int freq=pq.top().first;
+        int element=pq.top().second;
+        while(freq>0){
+            v.push_back(-1*element);
+            freq--;
+        }
+        pq.pop();
+    }
+    return v;
 }
-return 0;
+int main()
+ {
+	//code
+	int t ; 
+	cin >>t;
+	while(t--){
+	    int n ;
+	    cin>>n;
+	    int arr[n];
+	    for(int i =0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    vector<int >V=Sortbyfreq(arr,n);
+	    for(int i=0;i<n;i++){
+	        cout<<V[i]<<" ";
+	        
+	    }
+	    cout<<endl;
+	}
+	return 0;
 }
