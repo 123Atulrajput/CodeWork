73379c8785c1c6d2d77fa534e57073c4994d4c83
Atul Singh <91182252+CoderBannaa@users.noreply.github.com>
2023-10-19 23:10:51 +0530

    Added solution - LeetHub

---
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    public:
+    void func( int i , int j, vector<vector<int>> &mat,
+    int n,vector<string > &ans , string move,  vector<vector<int >>vis){
+        if(i==n-1&&j==n-1){
+            ans.push_back(move);
+            return ;
+        }
+
+        //downward
+        if(i+1<n && vis[i+1][j]==0 && mat[i+1][j]==1){
+            vis[i][j]=1;
+            func(i+1,j,mat,n,ans,move+'D',vis);
+            vis[i][j]=0;
+            
+        }
+        //right
+        if(j+1<n && mat[i][j+1]==1 && vis[i][j+1]==0){
+            vis[i][j]=1;
+            func(i,j+1,mat,n,ans,move+'R',vis);
+            vis[i][j]=0;
+        }
+        
+        //Left   vis [ i+/- 1][ or j +-] is due lots of dry run many cases can occur 
+         if(j-1>=0 && mat[i][j-1]==1 && vis[i][j-1]==0){
+            vis[i][j]=1;
+            func(i,j-1,mat,n,ans,move+'L',vis);
+            vis[i][j]=0;// yah toh path nhi sai tha yah dusra bhi path hai 
+        }
+        //upward
+         if(i-1>=0 && mat[i-1][j]==1 && vis[i-1][j]==0){
+            vis[i][j]=1;
+            func(i-1,j,mat,n,ans,move+'U',vis);
+            vis[i][j]=0;
+        }
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        // Your code goes here
+         vector < string > ans;
+      vector < vector < int >> vis(n, vector < int > (n, 0));
+
+      if (m[0][0] == 1) func(0, 0, m, n, ans, "", vis);
+      return ans;
+    }
+};
+
+    
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
